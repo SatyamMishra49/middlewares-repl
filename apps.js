788e63ffee75460e8e898d8dea24652fdbd83e87
Satyam Kumar Mishra <115921661+SatyamMishra49@users.noreply.github.com>
2024-02-29 09:24:58 +0000

    global catches

--- apps.js
+++ apps.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+app.use(express.json());
 function usermiddleware(req, res, next) {
   if (username != "sam" && password != "admin") {
     res.status(403).json({ msg: "Invalid User" });
@@ -12,5 +12,8 @@ function usermiddleware(req, res, next) {
 app.get("/odd", usermiddleware, function (req, res) {
   res.send("You are all set to go....");
 });
+app.use(err, req, res, next){
+  res.json({msg:"Error"});
+}
 
 app.listen(3001);
